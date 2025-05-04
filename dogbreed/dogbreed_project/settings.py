ea2edcf8892dff2f37361de49ccee5a70c24f961
hamistani <123585710+hamistani@users.noreply.github.com>
2025-05-04 16:07:49 -0700

    Database fix

--- dogbreed/dogbreed_project/settings.py
+++ dogbreed/dogbreed_project/settings.py
@@ -75,17 +75,21 @@ WSGI_APPLICATION = 'dogbreed.dogbreed_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql', 
-        'NAME': 'dog',
-        'USER': 'n8vea',
-        'PASSWORD': '',
-        'HOST': 'localhost', 
-        'PORT': '5432', 
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql', 
+#         'NAME': 'dog',
+#         'USER': 'n8vea',
+#         'PASSWORD': '',
+#         'HOST': 'localhost', 
+#         'PORT': '5432', 
+#     }
+# }
 
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://n8vea:password@localhost:5432/dog')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
